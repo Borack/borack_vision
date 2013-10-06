4ecb23346f1774536916250004fd8b48a3824197
Borack <borack@gmail.com>
2013-10-06 16:41:48 +0200

    window added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
-
+#include <QGuiApplication>
+#include <QQuickView>
+#include <QQmlApplicationEngine>
 
 
 int main(int argc, char *argv[])
 {
-    return 0;
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine(QUrl("qrc:///qml/main.qml"));
+//    engine.set
+
+//    QQuickView viewer;
+
+//    viewer.setSource(QUrl(()));
+//    viewer.show();
+
+
+    return app.exec();
+
 }
---
+++ src/qml/main.qml
@@ -0,0 +1,12 @@
+import QtQuick        2.0
+import QtQuick.Window 2.0
+
+Window {
+    id: win1;
+    width: 320;
+    height: 240;
+    visible: true;
+    color: "yellow";
+    title: "First Window";
+
+}

c41e83d2c8593a75789a38e86f0785562e016faa
Borack <borack@gmail.com>
2014-01-25 17:51:45 +0100

    Only the scene position.

--- src2/customscene.cpp
+++ src2/customscene.cpp
@@ -9,11 +9,11 @@ CustomScene::CustomScene(QObject* parent)
 
 void CustomScene::mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent)
 {
-   qDebug() << "Button down pos: " << mouseEvent->buttonDownPos(Qt::AllButtons);
-   qDebug() << "Button down scene pos: " << mouseEvent->buttonDownScenePos(Qt::AllButtons);
-   qDebug() << "Button down screen pos: " << mouseEvent->buttonDownScreenPos(Qt::AllButtons);
-   qDebug() << "Pos: " << mouseEvent->pos();
+//   qDebug() << "Button down pos: " << mouseEvent->buttonDownPos(Qt::AllButtons);
+//   qDebug() << "Button down scene pos: " << mouseEvent->buttonDownScenePos(Qt::AllButtons);
+//   qDebug() << "Button down screen pos: " << mouseEvent->buttonDownScreenPos(Qt::AllButtons);
+//   qDebug() << "Pos: " << mouseEvent->pos();
    qDebug() << "Scene pos: " << mouseEvent->scenePos();
-   qDebug() << "Screen pos: " << mouseEvent->screenPos();
-   qDebug() << "";
+//   qDebug() << "Screen pos: " << mouseEvent->screenPos();
+//   qDebug() << "";
 }

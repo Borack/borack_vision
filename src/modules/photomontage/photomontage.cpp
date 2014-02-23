2bfa9359a4cadad9b1a470ac4bf12eb0ffc1f61d
Borack <borack@gmail.com>
2014-02-23 19:32:55 +0100

    On the way to make seperate libs for each module

---
+++ src/modules/photomontage/photomontage.cpp
@@ -0,0 +1,6 @@
+#include "photomontage.hpp"
+
+PhotoMontage::PhotoMontage(QObject *parent) :
+   QObject(parent)
+{
+}
---
+++ src/modules/photomontage/photomontage.hpp
@@ -0,0 +1,18 @@
+#ifndef PHOTOMONTAGE_HPP
+#define PHOTOMONTAGE_HPP
+
+#include <QObject>
+
+class PhotoMontage : public QObject
+{
+   Q_OBJECT
+public:
+   explicit PhotoMontage(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // PHOTOMONTAGE_HPP

f9c1adca993cd1a723a242074a2187dfcae8b4dc
Borack <borack@gmail.com>
2014-03-14 21:25:16 +0100

    Drawing the strokes.

--- src/modules/photomontage/pm_sourcescene.cpp
+++ src/modules/photomontage/pm_sourcescene.cpp
@@ -1,5 +1,64 @@
 #include "pm_sourcescene.hpp"
+#include <QPainterPath>
+#include <QGraphicsSceneMouseEvent>
+#include <QDebug>
 
+//-----------------------------------------------------------------------------
 PMSourceScene::PMSourceScene(QObject *parent)
+   : CustomScene(parent)
+   , m_pathMode(EPathMode_Curve)
 {
+}
+
+//-----------------------------------------------------------------------------
+void PMSourceScene::mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent)
+{
+   m_currentStroke.clear();
+}
+
+//-----------------------------------------------------------------------------
+void PMSourceScene::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
+{
+   if(event->buttons() != Qt::NoButton)
+   {
+      m_currentStroke << event->scenePos();
+      drawStrokes();
+   }
+}
+
+//-----------------------------------------------------------------------------
+void PMSourceScene::mouseReleaseEvent(QGraphicsSceneMouseEvent *)
+{
+   m_allStrokes << m_currentStroke;
+   m_currentStroke.clear();
+}
+
+//-----------------------------------------------------------------------------
+void PMSourceScene::drawStrokes()
+{
+   QPainterPath path(m_currentStroke.at(0));
+
+   if (m_pathMode == EPathMode_Line) {
+       for (int i=1; i<m_currentStroke.size(); ++i) {
+           path.lineTo(m_currentStroke.at(i));
+       }
+   } else {
+       int i=1;
+       while (i + 2 < m_currentStroke.size()) {
+           path.cubicTo(m_currentStroke.at(i), m_currentStroke.at(i+1), m_currentStroke.at(i+2));
+           i += 3;
+           qDebug() << "in curved mode";
+       }
+       while (i < m_currentStroke.size()) {
+           path.lineTo(m_currentStroke.at(i));
+           ++i;
+       }
+   }
+   addPath(path,QPen(Qt::green, 5));
+}
+
+//-----------------------------------------------------------------------------
+void PMSourceScene::reset()
+{
+   m_allStrokes.clear();
 }
--- src/modules/photomontage/pm_sourcescene.hpp
+++ src/modules/photomontage/pm_sourcescene.hpp
@@ -7,14 +7,26 @@ class PMSourceScene : public CustomScene
 {
    Q_OBJECT
 public:
+   typedef QVector<QPointF> Stroke;
+   typedef QVector<Stroke> Strokes;
+   enum EPathMode { EPathMode_Curve, EPathMode_Line };
+
    explicit PMSourceScene(QObject *parent = 0);
 
-   virtual void reset() {}
+   virtual void reset();
 
 protected:
-   virtual void mousePressEvent(QGraphicsSceneMouseEvent * mouseEvent) {}
-   virtual void mouseMoveEvent(QGraphicsSceneMouseEvent *event) {}
-   virtual void mouseReleaseEvent(QGraphicsSceneMouseEvent *) {}
+   virtual void mousePressEvent(QGraphicsSceneMouseEvent * mouseEvent);
+   virtual void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
+   virtual void mouseReleaseEvent(QGraphicsSceneMouseEvent *);
+
+private:
+
+   void drawStrokes();
+
+   EPathMode m_pathMode;
+   Stroke m_currentStroke;
+   Strokes m_allStrokes;
 };
 
 #endif // PHOTOMONTAGE_SOURCESCENE_HPP

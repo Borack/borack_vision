1328bc615a40b4de80aa16221ec4465f8fe51d51
Borack <borack@gmail.com>
2014-05-04 23:33:57 +0200

    Data term added.

--- src/modules/photomontage/pm_window.cpp
+++ src/modules/photomontage/pm_window.cpp
@@ -7,7 +7,15 @@
 #include <QDebug>
 #include <QVariant>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <converter.hpp>
+
+#include <GCoptimization.h>
+
 #include <assert.h>
+#include <math.h>
 
 PmWindow::PmWindow(QWidget *parent) :
    QMainWindow(parent),
@@ -74,6 +82,7 @@ void PmWindow::setupCombobox()
 void PmWindow::runLuminance(bool isMinimum)
 {
    // FIXME: for now everything is hardcoded to 2 input images => 2 labels
+   const int num_labels = 2;
 
    PixmapPointer pix1 = ui->pmWidget->getPixmap();
    PixmapPointer pix2 = ui->pmWidget_2->getPixmap();
@@ -84,21 +93,59 @@ void PmWindow::runLuminance(bool isMinimum)
    assert(pix1->height() == pix2->height());
 
 
-   PMSourceScene::Strokes strokes1 = ui->pmWidget->strokes();
-   PMSourceScene::Strokes strokes2 = ui->pmWidget_2->strokes();
+   std::vector<PMSourceScene::Strokes> allImages;
+   allImages.push_back(ui->pmWidget->strokes());
+   allImages.push_back(ui->pmWidget_2->strokes());
 
+   cv::Mat mat1 = Converter::QPixmapToCvMatToCvMat(*pix1);
+   cv::Mat gray1;
+   cv::cvtColor(mat1, gray1, CV_BGR2GRAY);
+   cv::Mat mat2 = Converter::QPixmapToCvMatToCvMat(*pix2);
+   cv::Mat gray2;
+   cv::cvtColor(mat2, gray2, CV_BGR2GRAY);
+
+   assert(gray1.cols == gray2.cols && gray1.rows == gray2.rows);
+
+   GCoptimizationGridGraph* gc = new GCoptimizationGridGraph(mat1.cols, mat1.rows, num_labels);
+
+   assert(gc->numSites() == gray1.cols*gray1.rows);
 
    // TODO:
    // 1) find for each point the minimum(maximum) luminance color
    // 2) Assign to each pixel at the strokes position the data cost.
    // 3) SmoothCost
    // 4) Run GC and generate composite.
-   foreach (PMSourceScene::Stroke stroke, strokes1)
+
+   // See here for some implementation details: http://grail.cs.washington.edu/projects/photomontage/release/
+
+   foreach(PMSourceScene::Strokes imageStrokes, allImages)
    {
-      qDebug() << "A new stroke: ";
-      foreach (QPointF point, stroke)
+      foreach (PMSourceScene::Stroke stroke, imageStrokes)
       {
-         qDebug() << "Point: "  << point;
+         qDebug() << "A new stroke: ";
+         foreach (QPointF point, stroke)
+         {
+            const int x = static_cast<int>(std::round(point.x()));
+            const int y = static_cast<int>(std::round(point.y()));
+            const int site = y*gray1.cols + x;
+
+            const int lum1 = gray1.at<uchar>(x,y);
+            const int lum2 = gray2.at<uchar>(x,y);
+
+            if(lum1 < lum2)
+            {
+               gc->setDataCost(site,0,0);
+               gc->setDataCost(site,1,std::abs(lum2-lum1));
+            }
+            else
+            {
+               gc->setDataCost(site,0,std::abs(lum1-lum2));
+               gc->setDataCost(site,1,0);
+            }
+
+
+            qDebug() << "Point: "  << point;
+         }
       }
    }
 }

581e6b5073f0453ef798c3b962c5103543fb92ac
Borack <borack@gmail.com>
2015-01-30 21:49:11 +0100

    Own camera app stub.

---
+++ standalone_tools/CameraApp/Camera/Camera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Camera
+//
+//  Created by Simon Willi on 30/01/15.
+//  Copyright (c) 2015 Simon Willi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ standalone_tools/CameraApp/Camera/Camera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Camera
+//
+//  Created by Simon Willi on 30/01/15.
+//  Copyright (c) 2015 Simon Willi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

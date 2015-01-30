5c670e5bf984c9087bed86151200d5f7bb0f4116
Borack <borack@gmail.com>
2015-01-30 16:21:38 +0100

    halftone dummy

---
+++ standalone_tools/halftoning/src/main.cpp
@@ -0,0 +1,15 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+
+   cv::Mat input; // input image with R rows and C columns
+   int64_t E_p_m_n =0; // sum of the errors propogated to position (m,n) due to prior assignments
+   int64_t E_g_m_n = 0; // total error generated at position (m,n)
+
+//   cv::imread()
+   std::cout << "argc: " << argc << std::endl;
+
+
+}
